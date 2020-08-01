3d6adfac6261b9491b50549b550c898f4304acab
Jarship <JohnArthurBishop@gmail.com>
2020-08-01 18:07:25 -0400

    Updates

---
+++ precache-manifest.c426b2b2a4d0331739816457d762a086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1e6836e43b02124d0fbad4b1f54e07",
+    "url": "/component-example/index.html"
+  },
+  {
+    "revision": "c046535a4bcb1865cce4",
+    "url": "/component-example/static/js/2.ac0f23af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/component-example/static/js/2.ac0f23af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8523340e4f75240bdb65",
+    "url": "/component-example/static/js/main.294ddb36.chunk.js"
+  },
+  {
+    "revision": "b114ca435a9a8f570bef",
+    "url": "/component-example/static/js/runtime-main.bacadf8f.js"
+  }
+]);
