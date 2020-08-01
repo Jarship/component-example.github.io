5917cd513525cfe693a0fcb26b6813b6950b2718
Jarship <JohnArthurBishop@gmail.com>
2020-08-01 17:33:16 -0400

    update App page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ComponentTitle, LogoCarousel} from "./components";
+
+
+const LogoItems = [
+  {
+    alignment: "center",
+    name: "Mittens",
+    description: "Mittens is a great kitten! Mittens loves to play with other kittens."
+  },
+  {
+    alignment: "center",
+    name: "Fluffy",
+    description: "Fluffy is a great kitten! Fluffy works hard and plays hard!"
+  },
+  {
+    alignment: "center",
+    name: "Roscoe",
+    description: "Roscoe is a great kitten! Roscoe likes to play fetch with his little ball."
+  },
+  {
+    alignment: "center",
+    name: "Avery",
+    description: "Avery is a great kitten! Avery likes to give love nibbles, and then lots of kisses."
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ComponentTitle bg="#f6f4f3">Light Kitten Carousel</ComponentTitle>
+      <LogoCarousel background="Light" logoItems={LogoItems} />
+      <ComponentTitle bg="#f6f4f3">Dark Kitten Carousel</ComponentTitle>
+      <LogoCarousel background="Dark" logoItems={LogoItems} />
+    </>
   );
 }
 
